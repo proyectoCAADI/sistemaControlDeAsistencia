e5e527b574bc1bd28b23edc588d810dce72422f6
Luis Enrique Pérez Rodríguez <le.perezrodriguez@ugto.mx>
2017-10-16 13:38:02 -0500

    Modal de Habilidades
    El usuario ingresa una vez su NUA, aparece el mensaje de bienvenida, vuelve a introducir su NUA y aparece un modal para seleccionar la habilidad trabajada, en caso de ingresar un NUA que no sea el de Walter (810243), manda un mensaje diciendo que no está registrado.

--- Migrate_Views/js/registerValidate.js
+++ Migrate_Views/js/registerValidate.js
@@ -1,3 +1,22 @@
+var flag1 = false;
+
+$(document).ready(function(){
+    var txtBox = document.getElementById("txtNUA");
+});
+
+function check(nua){
+    if(( nua.value == 810243 )  && ( !flag1 )){
+        openWelcomeModal();
+        flag1 = true;
+    }
+    else if ( nua.value == 810243 ){
+        openSkillsModal();
+        flag1 = false;
+    }
+    else{
+        notUser();
+    }
+}
 
 function validate(){
     var nua = document.getElementById("txtNUA");
@@ -18,12 +37,68 @@ function validate(){
         if( digits.length != 6 ){
             err2.style.display = "block";
             nua.value = '';
-        }else
+        }else{
             err2.style.display = "none";
+            check(nua);
+        }
     }
+}
+
+function skill(value){
+
+    if( value == 'Grammar'){
+        closeSkillsModal();
+        alert('Habilidad: Grammar');
+    }else if( value == 'Listening'){
+        closeSkillsModal();
+        alert('Habilidad: Listening');
+    }else if( value == 'Reading'){
+        closeSkillsModal();
+        alert('Habilidad: Reading');
+    }else if( value == 'Speaking'){
+        closeSkillsModal();
+        alert('Habilidad: Speaking');
+    }
+
+    document.getElementById("txtNUA").value = '';
+}
+
+function openWelcomeModal(){
+    var modal = document.getElementById("welcomeModal");
+
+    modal.style.display = "block";
 }
 
 function closeWelcomeModal(){
     var modal = document.getElementById("welcomeModal");
     modal.style.display = "none";
+    document.getElementById("txtNUA").value = '';
+}
+
+function openSkillsModal(){
+    var modal = document.getElementById("skillsModal");
+
+    modal.style.display = "block";
+}
+
+function closeSkillsModal(){
+    var modal = document.getElementById("skillsModal");
+    modal.style.display = "none";
+
+    document.getElementById("txtNUA").value = '';
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById("welcomeModal");
+    
+    if (event.target === modal ) {
+        modal.style.display = "none";
+    }
+
+    document.getElementById("txtNUA").value = '';
+}
+
+function notUser(){
+    document.getElementById("txtNUA").value = '';
+    alert('No eres un usuario registrado');
 }
