8811a61a11d1818d556226369e31ff22f0c317c4
wamor17 <wa.morenoramirez@ugto.mx>
2017-11-22 02:12:27 -0600

    Estilos para los usuarios logeados
    Se agregaron los estilos para la tabla que muestra los usuarios logeados en el sistema. Tambi[en se agregó un menú y un nuevo header para la vista de esta funcionalidad.

--- CaadiMigrate/target/generated-sources/annotations/com/CaadiTables/Entities/Visit_.java
+++ CaadiMigrate/target/generated-sources/annotations/com/CaadiTables/Entities/Visit_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2017-11-19T01:31:11")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2017-11-21T23:39:42")
 @StaticMetamodel(Visit.class)
 public class Visit_ { 
 
