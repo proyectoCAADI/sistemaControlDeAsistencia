860cfd47947332cf514853e6a6b89858f7973a74
wamor17 <wa.morenoramirez@ugto.mx>
2017-12-10 00:16:53 -0600

    Estilos generales en versión funcional
    Se agregaron los estilos para una versión estable del proyecto.

--- CaadiMigrate/target/CaadiMigrate-1.0-SNAPSHOT/resources/js/RegisterValidate.js
+++ CaadiMigrate/target/CaadiMigrate-1.0-SNAPSHOT/resources/js/RegisterValidate.js
@@ -1,31 +1,12 @@
-var flag1 = false;
 
 $(document).ready(function(){
-    var txtBox = document.getElementById("txtNUA");
+    document.getElementById("txtNUA").value = '';
 });
 
-function check(nua){
-    if(( nua.value == 810243 )  && ( !flag1 )){
-        openWelcomeModal();
-        flag1 = true;
-    }
-    else if ( nua.value == 810243 ){
-        openSkillsModal();
-        flag1 = false;
-    }
-    else{
-        notUser();
-    }
+function openModal(){
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
 }
-
-function onfocus(){
-    var err1 = document.getElementById("errMessage1");
-    var err2 = document.getElementById("errMessage2");
-
-    err1.style.display = 'none';
-    err2.style.display = 'none';
-}
-
 
 function validateEntrada(){
     var nua = document.getElementById("formulario:txtNUA");
@@ -35,71 +16,77 @@ function validateEntrada(){
 }
 
 function limpiarTxtNUA(){
-
     document.getElementById("formulario:txtNUA").value = '';
 }
-
 
 function validateSalida(){
     var nua = document.getElementById("formulario:txtNUA");
 
     if(  nua.value !== "" )
         PF("salir").show();
-    }
-
-
-function skill(value){
-
-    if( value == 'Grammar'){
-        closeSkillsModal();
-        alert('Habilidad: Grammar');
-    }else if( value == 'Listening'){
-        closeSkillsModal();
-        alert('Habilidad: Listening');
-    }else if( value == 'Reading'){
-        closeSkillsModal();
-        alert('Habilidad: Reading');
-    }else if( value == 'Speaking'){
-        closeSkillsModal();
-        alert('Habilidad: Speaking');
-    }
-
-    document.getElementById("txtNUA").value = '';
 }
 
 function openWelcomeModal(){
     var modal = document.getElementById("welcomeModal");
-
     modal.style.display = "block";
 }
 
 function closeWelcomeModal(){
     var modal = document.getElementById("welcomeModal");
     modal.style.display = "none";
-    document.getElementById("txtNUA").value = '';
+}
+
+function openWelcome(){
+    var modal = document.getElementById("welcome");
+    modal.style.display = "block";
+    closeWelcomeModal();
+}
+
+function closeWelcome(){
+    var modal = document.getElementById("welcome");
+    modal.style.display = "none";
+    document.getElementById("formulario:txtNUA").value = '';
 }
 
 function openSkillsModal(){
     var modal = document.getElementById("skillsModal");
 
     modal.style.display = "block";
+    closeVerifyBook();
+    closeRegisterBook();
 }
 
 function closeSkillsModal(){
     var modal = document.getElementById("skillsModal");
     modal.style.display = "none";
 
-    document.getElementById("txtNUA").value = '';
+    document.getElementById("formulario:txtNUA").value = '';
 }
 
-window.onclick = function(event) {
-    var modal = document.getElementById("welcomeModal");
-    
-    if (event.target === modal ) {
-        modal.style.display = "none";
-    }
+function openVerifyBook(){
+    var modal = document.getElementById("verifyBook");
 
-    document.getElementById("txtNUA").value = '';
+    modal.style.display = "block";
+}
+
+function closeVerifyBook(){
+    var modal = document.getElementById("verifyBook");
+    modal.style.display = "none";
+    openRegisterBook();
+}
+
+function openRegisterBook(){
+    var modal = document.getElementById("registerBook");
+
+    modal.style.display = "block";
+}
+
+function closeRegisterBook(){
+    var modal = document.getElementById("registerBook");
+    modal.style.display = "none";
+
+    document.getElementById("formulario:txtNUA").value = '';
+    openSkillsModal();
 }
 
 function notUser(){
